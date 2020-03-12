98ea048a2b7b459c1b99318beb187cd4ed783462
Marcus Vinicius Patriota <>
2020-03-11 22:35:29 -0300

    TESTE JACOCO

---
+++ app/src/androidTest/java/cesar/devapps/finalproject/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package cesar.devapps.finalproject
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import cesar.devapps.finalproject.views.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+}
